72211660fdb4b8d4f0f5d85bd1845aa456c225f1
steffensc <schmelter.steffen@googlemail.com>
2023-06-30 13:06:22 +0200

    Update config.h

--- include/config.h
+++ include/config.h
@@ -1,9 +1,14 @@
 #pragma once 
 
-// WiFi Configuration //
-const char *WIFI_SSID     = "WiFi SSid";
-const char *WIFI_PASSWORD = "Password";
+// Initial Default Values (Configured in EEPROM) //
+const String DEFAULT_WIFI_SSID     = "WiFi SSID";
+const String DEFAULT_WIFI_PASSWORD = "Password";
+const String DEFAULT_SENSORNO      = "00";
+const float  DEFAULT_TEMP_BIAS     = 0.0;
+const float  DEFAULT_HUMI_BIAS     = 0.0;
 
+// WiFi HotSpot Setup Password //
+const String WIFI_SETUP_PASS = "123456789";
 
 // PHP API Server Endpoint Configuration //
 const String PHPAPI_SERVER_URL   = "http://webserverurl.de/temperature/write_data.php";
@@ -11,7 +16,3 @@ const String PHPAPI_SERVER_TOKEN = "tokenstring";
 
 // Homebridge Server Webhook Configuration //
 const String HOMEBRIDGE_WEBHOOK_URL   = "http://192.168.1.42:51828/";
-
-
-// Device Configuration
-const String SENSOR_NO = "10"; // Watch out for keeping this unique
