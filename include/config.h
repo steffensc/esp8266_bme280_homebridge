cf55e9abcaf1e6ede594af019d6376327744bfec
steffensc <schmelter.steffen@googlemail.com>
2023-06-30 13:06:57 +0200

    Update config.h

--- include/config.h
+++ include/config.h
@@ -8,11 +8,11 @@ const float  DEFAULT_TEMP_BIAS     = 0.0;
 const float  DEFAULT_HUMI_BIAS     = 0.0;
 
 // WiFi HotSpot Setup Password //
-const String WIFI_SETUP_PASS = "123456789";
+const String WIFI_SETUP_PASS       = "123456789";
 
 // PHP API Server Endpoint Configuration //
-const String PHPAPI_SERVER_URL   = "http://webserverurl.de/temperature/write_data.php";
-const String PHPAPI_SERVER_TOKEN = "tokenstring";
+const String PHPAPI_SERVER_URL     = "http://webserverurl.de/temperature/write_data.php";
+const String PHPAPI_SERVER_TOKEN   = "tokenstring";
 
 // Homebridge Server Webhook Configuration //
-const String HOMEBRIDGE_WEBHOOK_URL   = "http://192.168.1.42:51828/";
+const String HOMEBRIDGE_WEBHOOK_URL = "http://192.168.1.42:51828/";
